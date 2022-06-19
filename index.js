89362baafcdb3226863aa879a8f0e0af10dd877d
rensa06 <junayedhossain77@gmail.com>
2022-06-19 12:52:12 +0600

    try again

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-const port = process.env.port || 3000;
+const port = 3000;
 app.listen(port, () => console.info(`server has started on port ${port}`));
